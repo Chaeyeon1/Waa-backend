4eee6c30c65f81d6c5834c43b76fafef2282e48a
Chaeyeon1 <66813821+Chaeyeon1@users.noreply.github.com>
2023-08-04 00:37:37 +0900

    migration 오류 수정

--- prisma/migrations/20230803150311_basic/migration.sql
+++ prisma/migrations/20230803150311_basic/migration.sql
@@ -1,14 +1,25 @@
 -- CreateTable
+CREATE TABLE `Todo` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(191) NOT NULL,
+    `content` VARCHAR(191) NULL,
+    `is_done` BOOLEAN NULL DEFAULT false,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
 CREATE TABLE `User` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `username` VARCHAR(191) NOT NULL,
+    `username` VARCHAR(191) NULL,
     `score` INTEGER NULL,
     `age` INTEGER NULL,
     `userId` VARCHAR(191) NOT NULL,
-    `password` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NULL,
     `accessToken` VARCHAR(191) NULL,
     `refreshToken` VARCHAR(191) NULL,
 
+    UNIQUE INDEX `User_userId_key`(`userId`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -17,7 +28,7 @@ CREATE TABLE `Counseling` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `time` DATETIME(3) NOT NULL,
     `sender` VARCHAR(191) NOT NULL,
-    `text` VARCHAR(191) NOT NULL,
+    `content` VARCHAR(191) NOT NULL,
     `user_id` INTEGER NULL,
 
     PRIMARY KEY (`id`)
@@ -26,8 +37,8 @@ CREATE TABLE `Counseling` (
 -- CreateTable
 CREATE TABLE `TwentyQuestion` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `sender` VARCHAR(191) NOT NULL,
     `time` DATETIME(3) NOT NULL,
+    `sender` VARCHAR(191) NOT NULL,
     `content` VARCHAR(191) NOT NULL,
     `user_id` INTEGER NULL,
 
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TodoModule } from './todo/todo.module';
 import { UsersModule } from './users/users.module';
 import { CounselingModule } from './counseling/counseling.module';
 import { TwentyQuestionModule } from './twenty-question/twenty-question.module';
@@ -10,7 +9,6 @@ import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    TodoModule,
     UsersModule,
     CounselingModule,
     TwentyQuestionModule,
