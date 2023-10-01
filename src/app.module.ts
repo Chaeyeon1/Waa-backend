02d9854b1063d1cc21fe30b64b46dd24a68bebfc
Chaeyeon1 <66813821+Chaeyeon1@users.noreply.github.com>
2023-10-02 02:38:56 +0900

    안 쓰는 모듈 삭제

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,6 @@ import { CounselingModule } from './counseling/counseling.module';
 import { TwentyQuestionModule } from './twenty-question/twenty-question.module';
 import { WordChainModule } from './word-chain/word-chain.module';
 import { AuthModule } from './auth/auth.module';
-import { GptModule } from './gpt/gpt.module';
 
 @Module({
   imports: [
@@ -15,7 +14,6 @@ import { GptModule } from './gpt/gpt.module';
     CounselingModule,
     TwentyQuestionModule,
     WordChainModule,
-    GptModule,
   ],
   controllers: [AppController],
   providers: [AppService],
