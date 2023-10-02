558df78e576f528feeb937bf736a3c5a9b61a0f6
Chaeyeon1 <66813821+Chaeyeon1@users.noreply.github.com>
2023-10-03 02:52:18 +0900

    Mailer Service 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { TwentyQuestionModule } from './twenty-question/twenty-question.module';
 import { WordChainModule } from './word-chain/word-chain.module';
 import { AuthModule } from './auth/auth.module';
 import { SendSMSModule } from './sendSMS/sendSMS.module';
-import { MailerModule } from '@nestjs-modules/mailer';
+import { MailerModule, MailerService } from '@nestjs-modules/mailer';
 import { MailController } from './mail/mail.controller';
 import { MailService } from './mail/mail.service';
 import { MailModule } from './mail/mail.module';
@@ -28,6 +28,7 @@ import { PrismaService } from './prisma.service';
   controllers: [AppController, MailController],
   providers: [
     AppService,
+    MailerService,
     MailService,
     CounselingService,
     PrismaService,
