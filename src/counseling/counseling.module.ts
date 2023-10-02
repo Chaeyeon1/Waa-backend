3e658bdffca8e0e7f2a750e1a597fe6ca033dfed
Chaeyeon1 <66813821+Chaeyeon1@users.noreply.github.com>
2023-10-03 02:54:58 +0900

    Mailer Service 추가

--- src/counseling/counseling.module.ts
+++ src/counseling/counseling.module.ts
@@ -3,9 +3,10 @@ import { CounselingController } from './counseling.controller';
 import { CounselingService } from './counseling.service';
 import { PrismaService } from 'src/prisma.service';
 import { MailService } from 'src/mail/mail.service';
+import { MailerService } from '@nestjs-modules/mailer';
 
 @Module({
   controllers: [CounselingController],
-  providers: [CounselingService, PrismaService, MailService],
+  providers: [CounselingService, PrismaService, MailService, MailerService],
 })
 export class CounselingModule {}
