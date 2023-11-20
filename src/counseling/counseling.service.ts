1fb0866b8ebfaaaab3b912d2c96cd89cbb1db990
Chaeyeon1 <66813821+Chaeyeon1@users.noreply.github.com>
2023-11-21 00:21:53 +0900

    sender가 나일 때만 키워드에 저장

--- src/counseling/counseling.service.ts
+++ src/counseling/counseling.service.ts
@@ -86,6 +86,7 @@ export class CounselingService {
   ): Promise<{ keyword: string; count: number }[]> {
     const keywords = await this.prismaService.counseling.findMany({
       where: {
+        sender: 'user',
         user_id: user.id,
       },
       select: { content: true },
